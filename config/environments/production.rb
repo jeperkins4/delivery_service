9bd54a6e7df03bd70010bff462a792fae18b0b4d
John Perkins <jeperkins4@gmail.com>
2016-04-20 10:53:08 -0400

    Fix to redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://doorstepdelivery.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'http://doorstepdelivery.herokuapp.com', /http:\/\/doorstepdelivery.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
